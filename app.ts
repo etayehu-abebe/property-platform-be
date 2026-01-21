d586c84b5a6a378e244a61c2e080db786346e80e
etayehu-abebe <etayehu2025@gmail.com>
2026-01-21 19:00:20 +0300

    Frontend url set

--- app.ts
+++ app.ts
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin:
     process.env.NODE_ENV === "production"
-      ? ["https://your-frontend-domain.com"] // Change for production
+      ? ["https://property-platform-fe.vercel.app/"] // Change for production
       : ["http://localhost:3000", "http://localhost:3001"], // Development
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
